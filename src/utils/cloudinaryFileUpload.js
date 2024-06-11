d8dbc3c92373d8894b28dce3bb895d6b65605b35
sunneysood <suny.sn1@gmail.com>
2024-06-12 00:42:17 +0530

    stable built commtited for user registration

--- src/utils/cloudinaryFileUpload.js
+++ src/utils/cloudinaryFileUpload.js
@@ -14,7 +14,8 @@ const uploadOnCloudinary = async (localFilePath) => {
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    console.log("File is successfully uploaded to Cloudinary", response.url);
+    //console.log("File is successfully uploaded to Cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath);
